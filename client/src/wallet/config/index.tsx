1fe4361db37ef45568dfd4c74f66c22daabd6918
Hrishikesh Rana <itsranahrishikesh@gmail.com>
2025-11-05 21:12:25 +0530

    Added wallet config

---
+++ client/src/wallet/config/index.tsx
@@ -0,0 +1,54 @@
+import { createAppKit } from '@reown/appkit/react'
+
+import { WagmiProvider } from 'wagmi'
+import { AppKitNetwork, mainnet, somniaTestnet } from '@reown/appkit/networks'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import React from 'react'
+
+// 0. Setup queryClient
+const queryClient = new QueryClient()
+
+// 1. Get projectId from https://dashboard.reown.com
+export const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+// 2. Create a metadata object - optional
+const metadata = {
+  name: 'SpearHead',
+  description: 'Wallet for the somnia game',
+  url: 'https://example.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+}
+
+// 3. Set the networks
+const networks = [mainnet,somniaTestnet] as [AppKitNetwork, ...AppKitNetwork[]]
+
+// 4. Create Wagmi Adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true
+})
+
+// 5. Create modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+export function AppKitProvider({ children }:{children:React.ReactNode}) {
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
